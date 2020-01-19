28c29c756163e839e5495c093798513e3d89c095
Sergiy Rudenko <silavsale@gmail.com>
2020-01-19 17:44:31 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>
+          Hello I am a DEVELOPER!
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
